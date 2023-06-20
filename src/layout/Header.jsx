bac51df3b4e6b9bf2bfa6ad321740ac8d6b18f25
Levis <miduwaystark@gmail.com>
2023-06-20 21:21:15 +0400

    first commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+	return (
+		<nav className='light-blue darken-1'>
+			<div className='nav-wrapper'>
+				<a
+					href='https://ligmanstark.github.io/movies-react-project/'
+					className='brand-logo'
+				>
+					Fortnite Shop
+				</a>
+
+				<ul
+					id='nav-mobile'
+					className='right hide-on-med-and-down'
+				>
+					<li>
+						<a href='https://ligmanstark.github.io/movies-react-project/'>
+							Gallery
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
+
+export default Header;
